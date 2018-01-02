72ab2a92dd279d86c727ad0a0930a2c8662dd0f7
RobinCamarasa <robin.camarasa@etu.emse.fr>
2018-01-02 18:58:32 +0100

    Testing drools 1.1

--- src/main/java/com/robin/camarasa/nutritivecoach/web/Controller/RateController.java
+++ src/main/java/com/robin/camarasa/nutritivecoach/web/Controller/RateController.java
@@ -2,9 +2,13 @@ package com.robin.camarasa.nutritivecoach.web.Controller;
 
 import com.robin.camarasa.nutritivecoach.dao.FoodDao;
 import com.robin.camarasa.nutritivecoach.model.Food;
+import com.robin.camarasa.nutritivecoach.model.Preferences;
 import com.robin.camarasa.nutritivecoach.web.dto.FoodDto;
 import com.robin.camarasa.nutritivecoach.web.dto.FoodcatDto;
 import com.robin.camarasa.nutritivecoach.web.dto.FoodnameDto;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,14 +25,27 @@ import java.util.stream.Collectors;
 public class RateController {
 
     private final FoodDao foodDao;
+    private KieServices ks = KieServices.Factory.get();
+    private KieContainer kContainer = ks.getKieClasspathContainer();
+    private KieSession kSession = kContainer.newKieSession("ksession-rate");
+
 
     public RateController(FoodDao foodDao) {
         this.foodDao = foodDao;
     }
 
     @GetMapping(value = "/all")
-    public String getallfood() {
-        return "test";
+    public List<String> getallfood() {
+        Preferences preferences = new Preferences(2.5f,null,null);
+        kSession.insert(preferences);
+        kSession.fireAllRules();
+        List<String> tests = new ArrayList<>();
+        for (Object o : kSession.getObjects()) {
+            if(o.getClass().equals(String.class)) {
+                tests.add((String) o);
+            }
+        }
+        return tests;
     }
 
 }
